921a94cf3a668e8e5a2dc479434e6dcb7b72bca3
Jeff Ballard <juggalobrotha@gmail.com>
2016-02-15 18:27:57 -0500

    Create spLoginGetUserID.sql

---
+++ DatabaseScripts/spLoginGetUserID.sql
@@ -0,0 +1,22 @@
+USE gr_jci;
+
+DELIMITER $$
+
+/* Get the UserID (or -1) for the EmailAddress/Password combination */
+DROP PROCEDURE IF EXISTS `spLoginGetUserID`$$
+CREATE PROCEDURE `spLoginGetUserID`(IN _EmailAddress VarChar(200), IN _Password VarChar(50))
+DETERMINISTIC
+BEGIN
+  Declare _UserID Int;
+
+  Select u.UserID Into _UserID
+  From Users u
+  Where u.EmailAddress = _EmailAddress
+    And u.PasswordHash = SHA1(_Password)
+	And u.Active = 1
+	And e.EmailStatus = 3;
+	
+  Select IfNull(_UserID, -1) As 'UserID';
+END$$
+
+DELIMITER ;
