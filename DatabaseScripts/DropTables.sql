ad240d06e4eb93547252ba85b83bc9ef57f1a241
Jeff Ballard <juggalobrotha@gmail.com>
2016-02-15 14:19:45 -0500

    Create DropTables.sql

---
+++ DatabaseScripts/DropTables.sql
@@ -0,0 +1,31 @@
+USE gr_jci;
+
+DROP PROCEDURE IF EXISTS `spGetUserID`;
+DROP PROCEDURE IF EXISTS `spGetUserRoles`;
+
+DROP TABLE IF EXISTS `SystemSettings_Email`;
+DROP TABLE IF EXISTS `SystemSettings_CaseDates`;
+DROP TABLE IF EXISTS `SubmissionFiles`;
+DROP TABLE IF EXISTS `ReviewerFiles`;
+DROP TABLE IF EXISTS `FileData`;
+DROP TABLE IF EXISTS `FileTypes`;
+DROP TABLE IF EXISTS `Files`;
+DROP TABLE IF EXISTS `SubmissionCategories`;
+DROP TABLE IF EXISTS `Reviewers`;
+DROP TABLE IF EXISTS `AuthorsSubmission`;
+DROP TABLE IF EXISTS `Submissions`;
+DROP TABLE IF EXISTS `UserRoles`;
+DROP TABLE IF EXISTS `Addresses`;
+DROP TABLE IF EXISTS `PhoneNumbers`;
+DROP TABLE IF EXISTS `UserMembershipHistory`;
+DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `AddressTypes`;
+DROP TABLE IF EXISTS `PhoneTypes`;
+DROP TABLE IF EXISTS `ReviewStatus`;
+DROP TABLE IF EXISTS `AccouncementRoles`;
+DROP TABLE IF EXISTS `Announcements`;
+DROP TABLE IF EXISTS `Categories`;
+DROP TABLE IF EXISTS `SubmissionStatus`;
+DROP TABLE IF EXISTS `States`;
+DROP TABLE IF EXISTS `EmailStatus`;
+DROP TABLE IF EXISTS `Roles`;
