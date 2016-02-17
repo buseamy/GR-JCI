935dd4de23700a87f52665ed1af52154ac928b42
Jeff Ballard <juggalobrotha@gmail.com>
2016-02-16 21:35:12 -0500

    Added spGetUsersList procedure

--- DatabaseScripts/CreateStoredProcedure.sql
+++ DatabaseScripts/CreateStoredProcedure.sql
@@ -76,4 +76,24 @@ BEGIN
   Select IfNull(_UserID, -1) As 'UserID';
 END$$
 
+/* Get the list of users, both active and inactive in alphbetical order */
+DROP PROCEDURE IF EXISTS `spGetUsersList`$$
+CREATE PROCEDURE `spGetUsersList`()
+DETERMINISTIC
+BEGIN
+  Select u.UserID,
+         u.EmailAddress,
+		 CONCAT(u.FirstName,' ',u.LastName) As 'Name',
+		 GROUP_CONCAT(r.RoleTitle) As 'Roles',
+		 IF(u.Active, 'Y', 'N') As 'IsActive'
+  From Users u
+    Inner Join UserRoles ur
+	  On ur.UserID = u.UserID
+	Inner Join Roles r
+	  On r.RoleID = ur.RoleID
+  Order By u.LastName,
+           u.FirstName,
+		   u.UserID;
+END$$
+
 DELIMITER ;
