864c83839a2a8e201a4d11cab0738933500301cb
Jeff Ballard <juggalobrotha@gmail.com>
2016-02-21 00:34:08 -0500

    Had omited the RemoveUserRole procedure

--- DatabaseScripts/CreateStoredProcedures.sql
+++ DatabaseScripts/CreateStoredProcedures.sql
@@ -414,4 +414,14 @@ BEGIN
     And NewEmailAddressCreateDate < CURRENT_DATE - INTERVAL 3 DAY;
 END$$
 
+/* Removes a UserID with a RoleID */
+DROP PROCEDURE IF EXISTS `spUserRemoveRole`$$
+CREATE PROCEDURE `spUserRemoveRole`(IN _UserID int, IN _RoleID int)
+DETERMINISTIC
+BEGIN
+  Delete From UserRoles
+  Where UserID = _UserID
+    And RoleID = _RoleID;
+END$$
+
 DELIMITER ;
