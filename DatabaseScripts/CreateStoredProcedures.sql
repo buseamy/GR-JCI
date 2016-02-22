95b79df01d9ad1097032025fe443862186cbfe64
Jeff Ballard <juggalobrotha@gmail.com>
2016-02-22 08:04:29 -0500

    Updated the AcceptEmailAddress procedure

--- DatabaseScripts/CreateStoredProcedures.sql
+++ DatabaseScripts/CreateStoredProcedures.sql
@@ -299,8 +299,17 @@ DROP PROCEDURE IF EXISTS `spUpdateAcceptEmailAddress`$$
 CREATE PROCEDURE `spUpdateAcceptEmailAddress`(IN _UserID int)
 DETERMINISTIC
 BEGIN
+  /* Copy the new email address into the EmailAddress field */
   Update Users
-  Set EmailStatusID = 3
+  Set EmailAddress = NewEmailAddress
+  Where UserID = _UserID;
+  
+  /* Mark the record's email address as valid */
+  Update Users
+  Set EmailStatusID = 3,
+      NewEmailAddress = Null,
+	  EmailVerificationGUID = Null,
+	  NewEmailAddressCreateDate = Null
   Where UserID = _UserID;
 END$$
 
