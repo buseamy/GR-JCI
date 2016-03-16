101b2b04890f01b7d1c8070c08484301047c0781
Jeff Ballard <juggalobrotha@gmail.com>
2016-03-16 12:51:29 -0400

    First implementation of CronJobs

--- CronJobs.php
+++ CronJobs.php
@@ -2,7 +2,24 @@
 require('mysqli_connect.php');
 require('public_html/include_utils/procedures.php');
 
-//$q_JobTest = 'Call spJobTest()';
-//$r_JobTest = mysqli_query($dbc, $q_JobTest);
-//complete_procedure($dbc);
+//Expire unverified email address changes
+mysqli_query($dbc, 'Call spJobUpdateExpireUsersEmailAddressChange()');
+complete_procedure($dbc);
+
+//Delete expired announcements
+mysqli_query($dbc, 'Call spJobRemoveExpiredAnnouncements()');
+complete_procedure($dbc);
+
+if ((date('n') == 1) && date('d') == 1) {
+  //Create the new Dates records for current year
+  mysqli_query($dbc, 'Call spJobCreateArticleDates()');
+  complete_procedure($dbc);
+}
+
+if ((date('n') == 12) && date('d') == 31) {
+  //Record user's MembershipCode history for current year
+  mysqli_query($dbc, 'Call spJobYearlyAddMembershipHistory()');
+  complete_procedure($dbc);
+}
+
 ?>
