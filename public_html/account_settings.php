43161f94209095416dae3ffb3123f67b1908bc95
ahmedj777 <ahmedj777@gmail.com>
2016-03-20 01:17:44 -0400

    updates

---
+++ public_html/account_settings.php
@@ -0,0 +1,9 @@
+<?php // account settings page created by Jamal Ahmed
+
+
+?>
+
+	<ul>
+		<li><href="user_update_password.php">Change Password</li>
+		<li><href="user_update_email.php">Change Email</li>
+	</ul>
