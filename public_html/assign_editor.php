6253efa53619f3846b63c5856682b3d4d93c803e
ahmedj777 <ahmedj777@gmail.com>
2016-03-20 00:09:16 -0400

    Updated

--- public_html/assign_editor.php
+++ public_html/assign_editor.php
@@ -26,35 +26,63 @@ if (!$error) {
 		$r_cases = @mysqli_query ($dbc, $q_cases);
 		if(mysqli_num_rows)($r_cases > 0) {
 
-	/*
-	    <form action="" method="post">
-    <table style="border: 1px solid black"> create table 
-	*/
-		while($row_cases = mysqli_fetch_array($r_cases, mysqli_ASSOC)) {
+		?>
+	
+	<form action="" method="post">
+    <table style="border: 1px solid black">
+	<?php
+	
+		while($row_cases = mysqli_fetch_array($r_cases, mysqli_ASSOC)) 
 				// print_r($row_cases);
 				// source http://stackoverflow.com/questions/6112875/display-sql-data-in-a-list-with-check-box
 				{
                 echo '<tr><td>';
-                echo '<input type="checkbox" name="selected[]" value="'.$row['id'].'"/>'; // replace id with primary key SubmissionID
+                echo '<input type="checkbox" name="selected[]" value="'.$row['SubmissionID'].'"/>'; // replace id with primary key SubmissionID
                 echo '</td>';
                 foreach ($row_cases as $key => $value)
                     echo '<td>'.htmlspecialchars($value).'</td>';
                 echo '</tr>';
             }
-			//</table>
-		
-		//<input type="submit"/>
-		//</form>
-		// SELECT * FROM table WHERE id = '.$_POST['selected'][i] where i is iterating through every element of array
-		
+		// http://stackoverflow.com/questions/4997252/get-post-from-multiple-checkboxes
+		  foreach($_POST['selected'] as $caseID) {
+            echo $caseID; //echoes the value set in the HTML form for each checked checkbox.
+                         //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
+                         //in this case, it would echo whatever $row['SubmissionID'] is equivalent to.
+    }
+			complete_procedure($dbc);
+			?>
+		</table>
+		<?php
+		$q_editors = " CALL spGetUsersEditorsList" ;
+		$r_editors = @mysqli_query ($dbc, $q_editors);
+		while($row_editors = mysqli_fetch_array($r_editors, mysqli_ASSOC))
+		{
+			echo '<input type="radio" name="editor" value="'.$row['UserID'].'"/>';
+			foreach ($row_editors as $key => $value)
+                echo '<td>'.htmlspecialchars($value).'</td>';
 		}
-		print_r($_POST);
-		$_POST['selected']
+		$editor_id = $_POST['editor'] ;
+		complete_procedure($dbc);
+		?>
+
+		<input type="submit"/>
+		</form>
+		<?php
+		
+		$q_assign_case = " spUpdateSubmissionAssignEditor('$caseID' ,'$editor_id') " ;
+		$r_assign_case = @mysqli_query ($dbc, $q_assign_case);
+		while($row_assign_case = mysqli_fetch_array($r_assign_case, mysqli_ASSOC)) {
+			echo 'The following cases have been assigned to the chosen editor' ;
+			print_r($row_assign_case);
+		}
+		
+		
+
 		}
 		else {
 		echo 'No cases submitted at this time' ;
 		}
-		complete_procedure($dbc);
+		
 		
 	}
 		
