a0f358091e7592d7b9eeea0b73028dc633c050d8
Jeff Ballard <juggalobrotha@gmail.com>
2016-03-23 21:18:11 -0400

    Added the new Publication tables from the ERD

--- DatabaseScripts/Database Structure/CreateDatabase.sql
+++ DatabaseScripts/Database Structure/CreateDatabase.sql
@@ -283,6 +283,47 @@ FOREIGN KEY (SubmissionID) REFERENCES Submissions(SubmissionID),
 FOREIGN KEY (FileMetaDataID) REFERENCES FileMetaData(FileMetaDataID)
 );
 
+/* Create the PublishedAuthors table */
+CREATE TABLE PublishedAuthors (
+AuthorID int NOT NULL AUTO_INCREMENT,
+FirstName VarChar(15) NOT NULL,
+LastName VarChar(30) NOT NULL,
+EmailAddress VarChar(200) NOT NULL,
+InstitutionAffiliation varchar(100),
+PRIMARY KEY (AuthorID)
+);
+
+/* Create the Publications table */
+CREATE TABLE Publications (
+PublicationID int NOT NULL AUTO_INCREMENT,
+Year int NOT NULL,
+FileMetaDataID int NOT NULL,
+PRIMARY KEY (PublicationID),
+FOREIGN KEY (FileMetaDataID) REFERENCES FileMetaData(FileMetaDataID)
+);
+
+/* Create the PublishedCriticalIncidents table */
+CREATE TABLE PublishedCriticalIncidents (
+CriticalIncidentID int NOT NULL AUTO_INCREMENT,
+PublicationID int NOT NULL,
+IncidentTitle VarChar(150) NOT NULL,
+Abstract VarChar(5000) NOT NULL,
+Keywords VarChar(5000) NOT NULL,
+FileMetaDataID int NOT NULL,
+PRIMARY KEY (CriticalIncidentID),
+FOREIGN KEY (PublicationID) REFERENCES Publications(PublicationID),
+FOREIGN KEY (FileMetaDataID) REFERENCES FileMetaData(FileMetaDataID)
+);
+
+/* Create the PublishedIncidentsAuthors table */
+CREATE TABLE PublishedIncidentsAuthors (
+AuthorID int NOT NULL,
+CriticalIncidentID int NOT NULL,
+PRIMARY KEY (AuthorID,CriticalIncidentID),
+FOREIGN KEY (AuthorID) REFERENCES PublishedAuthors(AuthorID),
+FOREIGN KEY (CriticalIncidentID) REFERENCES PublishedCriticalIncidents(CriticalIncidentID)
+);
+
 /*Create SystemSettings_Email table*/
 CREATE TABLE SystemSettings_Email
 (
