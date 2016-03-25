5b5d08bb83e67e22d0e3a31e34eec906cfb3ffea
Jeff Ballard <juggalobrotha@gmail.com>
2016-03-24 20:49:58 -0400

    Updated the CronJobs to do "end of year" stuff a few days after the publication date

--- CronJobs.php
+++ CronJobs.php
@@ -10,15 +10,17 @@ complete_procedure($dbc);
 mysqli_query($dbc, 'Call spJobRemoveExpiredAnnouncements()');
 complete_procedure($dbc);
 
-if ((date('n') == 1) && date('d') == 1) {
-  //Create the new Dates records for current year
-  mysqli_query($dbc, 'Call spJobCreateArticleDates()');
-  complete_procedure($dbc);
-}
+//Find out if end of publish things need to happen yet
+$r = mysqli_fetch_array(mysqli_query($dbc, 'Call spJobPublishEndRollOver();'), MYSQLI_ASSOC);
+complete_procedure($dbc);
 
-if ((date('n') == 12) && date('d') == 31) {
+if ($r["RollOver"] == 1) {
   //Record user's MembershipCode history for current year
   mysqli_query($dbc, 'Call spJobYearlyAddMembershipHistory()');
+  complete_procedure($dbc);
+  
+  //Create the new Dates records for current year
+  mysqli_query($dbc, 'Call spJobCreateArticleDates()');
   complete_procedure($dbc);
 }
 
