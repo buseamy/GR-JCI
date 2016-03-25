a6d93b2f8d06580bc0d244cef595ac81d9a2b4b3
Jeff Ballard <juggalobrotha@gmail.com>
2016-03-24 21:21:29 -0400

    Removed spUpdateAcceptEmailAddress as a more appropriate one will handle acceptance

--- DatabaseScripts/CreateStoredProcedures.sql
+++ DatabaseScripts/CreateStoredProcedures.sql
@@ -310,25 +310,6 @@ BEGIN
   Where PhoneNumberID = _PhoneNumberID;
 END$$
 
-/* Marks a user's email address as valid */
-DROP PROCEDURE IF EXISTS `spUpdateAcceptEmailAddress`$$
-CREATE PROCEDURE `spUpdateAcceptEmailAddress`(IN _UserID int)
-DETERMINISTIC
-BEGIN
-  /* Copy the new email address into the EmailAddress field */
-  Update Users
-  Set EmailAddress = NewEmailAddress
-  Where UserID = _UserID;
-  
-  /* Mark the record's email address as valid */
-  Update Users
-  Set EmailStatusID = 3,
-      NewEmailAddress = Null,
-	  EmailVerificationGUID = Null,
-	  NewEmailAddressCreateDate = Null
-  Where UserID = _UserID;
-END$$
-
 /* Gets the List of available roles */
 DROP PROCEDURE IF EXISTS `spGetRoles`$$
 CREATE PROCEDURE `spGetRoles`()
