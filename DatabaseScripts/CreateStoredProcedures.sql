0e3e4ed23f9370400973fb2e94cbb35b6f8222cf
Jeff Ballard <juggalobrotha@gmail.com>
2016-03-29 15:22:39 -0400

    Updated adding author to a submission

--- DatabaseScripts/CreateStoredProcedures.sql
+++ DatabaseScripts/CreateStoredProcedures.sql
@@ -47,19 +47,34 @@ END$$
 /* Adds an Author UserID to an existing Submission */
 DROP PROCEDURE IF EXISTS `spAuthorAddToSubmission`$$
 CREATE PROCEDURE `spAuthorAddToSubmission`(IN _UserID int,
-                                           IN _SubmissionID int)
+                                           IN _SubmissionID int,
+										   IN _PrimaryContact tinyint)
 DETERMINISTIC
 BEGIN
-  Declare _InstitutionAffiliation varchar(100);
+  Declare _InstitutionAffiliation varchar(150);
+  Declare _AuthorSeniority int;
 	
   /* Make sure the UserID exists */
   If(Select Exists(Select 1 From Users Where UserID = _UserID)) Then
     If(Select Exists(Select 1 From Submissions Where SubmissionID = _SubmissionID)) Then
 	  
+	  Set _PrimaryContact = IfNull(_PrimaryContact, 0);
+	  
 	  /* Get the user's InstitutionAffiliation */
 	  Select InstitutionAffiliation Into _InstitutionAffiliation
 	  From Users
 	  Where UserID = _UserID;
+	  
+	  /* Get the highest author senority for the submission */
+	  Select Max(AuthorSeniority) + 1 Into _AuthorSeniority
+	  From AuthorsSubmission
+	  Where SubmissionID = _SubmissionID;
+	  
+	  If (_PrimaryContact = 1) Then
+	    Update AuthorsSubmission
+		Set PrimaryContact = 0
+		Where SubmissionID = _SubmissionID;
+	  End If;
 	  
 	  /* Link the UserID to the SubmissionID */
 	  Insert Into AuthorsSubmission (UserID,
@@ -70,8 +85,8 @@ BEGIN
 	  Values (_UserID,
 	          _SubmissionID,
 	  		  _InstitutionAffiliation,
-	  		  1,
-	  		  1);
+	  		  _PrimaryContact,
+	  		  _AuthorSeniority);
 	Else
 	  Select 'SubmissionID doesn''t exist' As 'Error';
 	End If;
