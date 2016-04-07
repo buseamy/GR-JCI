8fa32df0e8afbc3f6b113ee7c067fa2b7843e69b
ahmedj777 <ahmedj777@gmail.com>
2016-04-07 19:39:07 -0400

    progress

--- public_html/assign_editor.php
+++ public_html/assign_editor.php
@@ -3,6 +3,7 @@
 $page_title = 'assign_editor' ;
 require('../mysqli_connect.php');
 require('./include_utils/procedures.php');
+require('./include_utils/files.php'); // for create_download_link
 $error = false;
 $errors = array();
 
@@ -16,107 +17,177 @@ $is_editor = false;
 	else {
 		$error = true;
 		array_push($errors, "This page can only be accessed by the Editor.");
-}                                 
+}              
+
+/*
+In PHP, there are three kind of arrays:
+
+Numeric array - An array with a numeric index
+Associative array - An array where each ID key is associated with a value
+Multidimensional array - An array containing one or more arrays
+*/                   
 
 if (!$error) {
 	
 	if ($is_editor) {
 		
 		if(isset($_POST['submit'])) {
-		// if isset post method file upload/download prototype
-		// spSubmissionGetInfo, spSubmissionGetFilesList
-		
-		
-		$q_cases = "CALL spEditorViewSubmissions" . date("Y") ;
-		$r_cases = @mysqli_query ($dbc, $q_cases);
-		if(mysqli_num_rows($r_cases) > 0) {
-
-		?>
-	
-	<form action="assign_editor.php" method="post">
-    <table style="border: 1px solid black">
-	<?php
-	
-		while($row_cases = mysqli_fetch_array($r_cases, mysqli_ASSOC)) 
-				// print_r($row_cases);
-				// source http://stackoverflow.com/questions/6112875/display-sql-data-in-a-list-with-check-box
-				{
-                echo '<tr><td>';
-                echo '<input type="checkbox" name="selected[]" value="'.$row['SubmissionID'].'"/>'; // replace id with primary key SubmissionID
-                echo '</td>';
-                foreach ($row_cases as $key => $value)
-                    echo '<td>'.htmlspecialchars($value).'</td>';
-                
-            }
-		// http://stackoverflow.com/questions/4997252/get-post-from-multiple-checkboxes
-		  foreach($_POST['selected'] as $caseID) {
-            // echo $caseID; //echoes the value set in the HTML form for each checked checkbox.
-                         //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
-                         //in this case, it would echo whatever $row['SubmissionID'] is equivalent to.
-    }
-			complete_procedure($dbc);
 			
-		  foreach($caseID){
-			$q_submission = "CALL spSubmissionGetInfo ($caseID)"
-			$r_submission = @mysqli_query ($dbc, $q_submission);
-			while($row_submission = mysqli_fetch_array($r_submission, mysqli_ASSOC)) {
-				echo '< name="case" value="'.$row['IncidentTitle'].'"/>';
-				$case_title = $_POST['case'];
-				echo '<td>';
-                echo $case_title; 
-                echo '</td>';
-				complete_procedure($dbc);
-			}
-			
-			$q_submission_file = "CALL spSubmissionGetFilesList ($caseID)"
-			$r_submission_file = @mysqli_query ($dbc, $q_submission_file);
-			while($row_submission_file = mysqli_fetch_array($r_submission_file, mysqli_ASSOC)) {
-				// download link taken from author_view_critical incident page
-				echo '< name="ID" value="'.$row['FileMetaDataID'].'"/>';
-				$file_ID = $_POST['ID'];
-				 echo "<td><a href='download.php?fid=$file_ID'>Download</a></td>";
-				 echo '</tr>';
-				 complete_procedure($dbc);
-			}
-		  }
-		  
-			?>
-		</table>
-		<?php
-		$q_editors = " CALL spGetUsersEditorsList" ;
-		$r_editors = @mysqli_query ($dbc, $q_editors);
-		while($row_editors = mysqli_fetch_array($r_editors, mysqli_ASSOC))
-		{
-			echo '<input type="radio" name="editor" value="'.$row['UserID'].'"/>';
-			foreach ($row_editors as $key => $value)
-                echo '<td>'.htmlspecialchars($value).'</td>';
-		}
-		$editor_id = $_POST['editor'] ;
-		complete_procedure($dbc);
-		?>
-
-		<input type="submit"/>
-		</form>
-		<?php
+			/*
+			foreach ($_POST as $field => $value) 
+			{   // echo "<p>$field, $value</p>";   
+			if ($field != 'submit' && $value != 'Record Results' && $value != 'empty') 
+			{    $source = substr($field, 8);    $item = $value;        $q_r = "CALL AddRecord('$item', '$source', '$time');";    $r_r = mysqli_query($dbc, $q_r);    if ($r_r) 
+			{     echo "<p>Source: $source and Item: $item successfully recorded.</p>";    }    else {     echo '<p class="error">';   
+			echo "Could not process Source $source: and Item: $item.</p>";    }        while (mysqli_more_results($dbc)) {     mysqli_next_result($dbc);    }   }  }
+			*/
 		
-		$q_assign_case = " spUpdateSubmissionAssignEditor('$caseID' ,'$editor_id') " ;
+		
+		
+		$q_assign_case = " CALL spUpdateSubmissionAssignEditor('$caseID' ,'$editor_id') " ;
 		$r_assign_case = @mysqli_query ($dbc, $q_assign_case);
 		while($row_assign_case = mysqli_fetch_array($r_assign_case, mysqli_ASSOC)) {
 			echo 'The following cases have been assigned to the chosen editor' ;
 			print_r($row_assign_case);
 		}
 		
+		}  // Everything above this is processing the page
+		
+		// Everything below is preparing to display the form
+		$q_cases = "CALL spEditorViewSubmissions" . date("Y") ;
+		$r_cases = @mysqli_query ($dbc, $q_cases);
+		if(mysqli_num_rows($r_cases) > 0) {
+
+	$case_list = array();
+		while($row_cases = mysqli_fetch_array($r_cases, mysqli_ASSOC)) 
+				
+				// source http://stackoverflow.com/questions/6112875/display-sql-data-in-a-list-with-check-box
+				{
+                array_push($case_list, $row_cases);
+				
+                // foreach ($row_cases as $key => $value)
+                   // echo '<td>'.htmlspecialchars($value).'</td>';
+                
+            }
+		// http://stackoverflow.com/questions/4997252/get-post-from-multiple-checkboxes
+		  
+    
+			complete_procedure($dbc);
+			foreach($case_list as $case_row) {
+					$caseID = $case_row['SubmissionID'] ;
+					
+				
+			$q_submission = "CALL spSubmissionGetInfo ($caseID)";
+			
+			$r_submission = @mysqli_query ($dbc, $q_submission);
+			while($row_submission = mysqli_fetch_array($r_submission, mysqli_ASSOC)) {
+				// echo '< name="case" value="'.$row_submission['IncidentTitle'].'"/>';
+				
+				$row_submission['SubmissionID'] = $caseID ; // check to see if this works
+				// array_push($row_submission, $caseID);
+				array_push($submission_list, $row_submission);
+				foreach($submission_list as $submission_row) {
+					$case_title = $submission_row['IncidentTitle'] ;
+				}
+				
+				complete_procedure($dbc);
+			}
+			 $submission_fileIDs = array();
+			$q_submission_file = "CALL spSubmissionGetFilesList ($caseID)";
+			$r_submission_file = @mysqli_query ($dbc, $q_submission_file);
+			while($row_submission_file = mysqli_fetch_array($r_submission_file, mysqli_ASSOC)) {
+				// download link taken from author_view_critical incident page
+				// echo '< name="ID" value="'.$row_submission_file['FileMetaDataID'].'"/>';
+				 // echo "<td><a href='download.php?fid=$file_ID'>Download</a></td>";
+				 //echo '</tr>';
+				 //array_push(first parameter is the array, second parameter is the value of what is being put in the first parameter)
+				 array_push($submission_fileIDs, $row_submission_file);
+				
+				 foreach($submission_file_list as $submission_file_row) {
+					 $file_id = $submission_file_row['FileMetaDataID'] ;
+				 }
+				 complete_procedure($dbc);
+			}
+			$submission_file_list[$caseID] = $submission_fileIDs;
+			// array_push($submission_file_list, $submission_fileIDs);
+			}
+		  
+		  
+			?>
+		
+		<?php
+		$q_editors = " CALL spGetUsersEditorsList" ;
+		$r_editors = @mysqli_query ($dbc, $q_editors);
+		while($row_editors = mysqli_fetch_array($r_editors, mysqli_ASSOC))
+		{
+			// echo '<input type="radio" name="editor" value="'.$row_editors['UserID'].'"/>';
+			array_push($editor_list, $row_editors); 
+			foreach($editor_list as $editor_row) {
+				$editor_name = $editor_row['FullName'] ;// full name is output same for editorID get userID from array
+				$editor_ID = $editor_row['UserID'];
+			}
+			// foreach ($row_editors as $key => $value)
+               // echo '<td>'.htmlspecialchars($value).'</td>';
+		}
+		// $editor_id = $_POST['editor'] ;
+		complete_procedure($dbc);
+		?>
+		<form action="assign_editor2.php" method="post">
+		<table style="border: 1px solid black">
+		<?php
+		echo '<tr><td>';
+		foreach($submission_list as $submission_row) {
+					$caseID = $submission_row['SubmissionID'] ;
+					$case_name = $submission_row['IncidentTitle'] ;
+				
+					
+					
+					
+		echo '<label for = "chk-case-'.$caseID.'">'.$case_name.'</label>';
+		echo '<input type="checkbox" id = "chk-case-'.$caseID.'" name="selected[]" value="'.$caseID.'"/>';
+		foreach($editor_list as $editor_row) {	
+		$editor_name = $editor_row['FullName'] ;
+		$editor_ID = $editor_row['UserID'];
+		// radio button goes here
+		echo '<label for = "radio-editor-'.$editor_ID.'">'.$editor_name.'</label>';
+		echo '<input type="radio" id = "radio-editor-'.$editor_ID.'" name="selected1" value="'.$editor_ID.'"/>';
+		// echo '<input type="radio" name="editor" value="'.$row_editors['UserID'].'"/>';
+		// case name editors file download
+		}
+		foreach($submission_file_list[$caseID] as $submission_file_row) {
+			$file_id = $submission_file_row['FileMetaDataID'] ;
+			$filename = $submission_file_row['FileName'] ;
+			$filesize = $submission_file_row['FileSize'] ;
+			create_download_link ($file_id, $filename, $filesize);
+			
+			// download link goes here create_download_link($file_id, $filename, $filesize)
+		}
+		
+		}
+        echo '</td>';
+		echo '</tr>';
+		?>
+		
+		</table>
+		<input type="submit"/>
+		</form>
+		
+		<?php
+		
+		
+		
 		
 
 		}
-		else {
-		echo 'No cases submitted at this time' ;
-		}
+	else {
+	echo 'No cases submitted at this time' ;
+	}
 		
-		}
+		
 	}
 		
 }
+	
 	
 	
 	
