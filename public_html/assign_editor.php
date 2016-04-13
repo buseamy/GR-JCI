d0cfeb0f759bd4a9d87f2c3357f7a2f92a58bf4b
MitchSpencerF17 <spencm17@ferris.edu>
2016-04-12 22:01:16 -0400

    code review - formatting readability

--- public_html/assign_editor.php
+++ public_html/assign_editor.php
@@ -18,13 +18,12 @@ $errors = array();
 // this code was taken from Mitch
 $is_editor = false;
 
-	if (isset($_SESSION['is_editor'])) {
+if (isset($_SESSION['is_editor'])) {
 	$is_editor = $_SESSION['is_editor'];
-	}
-	
-	else {
-		$error = true;
-		array_push($errors, "This page can only be accessed by the Editor.");
+}
+else {
+    $error = true;
+    array_push($errors, "This page can only be accessed by the Editor.");
 }              
 
 /*
@@ -41,11 +40,8 @@ Multidimensional array - An array containing one or more arrays
 */                   
 
 if (!$error) {
-	
 	if ($is_editor) {
-		
 		if(isset($_POST['submit'])) {
-			
 			/*
 			foreach ($_POST as $field => $value) {   
 				// echo "<p>$field, $value</p>";   
@@ -61,17 +57,13 @@ if (!$error) {
 			}  
 			}
 			*/
-		
-		
-		
-		$q_assign_case = " CALL spUpdateSubmissionAssignEditor('$caseID' ,'$editor_id') ;" ;
-		$r_assign_case = @mysqli_query ($dbc, $q_assign_case);
-		while($row_assign_case = mysqli_fetch_array($r_assign_case, MYSQLI_ASSOC)) {
-			echo 'The following cases have been assigned to the chosen editor' ;
-			print_r($row_assign_case);
-		}
-		
-		}  
+            $q_assign_case = " CALL spUpdateSubmissionAssignEditor('$caseID' ,'$editor_id') ;" ;
+            $r_assign_case = @mysqli_query ($dbc, $q_assign_case);
+            while($row_assign_case = mysqli_fetch_array($r_assign_case, MYSQLI_ASSOC)) {
+                echo 'The following cases have been assigned to the chosen editor' ;
+                print_r($row_assign_case);
+            }
+        }
 		// Everything above this is processing the page
 		
 		// Everything below is preparing to display the form
@@ -83,26 +75,17 @@ if (!$error) {
 		$r_cases = @mysqli_query ($dbc, $q_cases);
 		if(mysqli_num_rows($r_cases) > 0) {
 
-		
-		while($row_cases = mysqli_fetch_array($r_cases, MYSQLI_ASSOC)) 
-				
-				
-				{
-				// Mitch helped me learn how to use array_push
-				//array_push(first parameter is the array, second parameter is the value of what is being put in the first parameter)
-                array_push($case_list, $row_cases);
-                
-            }
-		
-		  
-    
-			complete_procedure($dbc);
-			foreach($case_list as $case_row) {
-					$caseID = $case_row['SubmissionID'] ;
-					
-				
+		while($row_cases = mysqli_fetch_array($r_cases, MYSQLI_ASSOC)) {
+            // Mitch helped me learn how to use array_push
+            //array_push(first parameter is the array, second parameter is the value of what is being put in the first parameter)
+            array_push($case_list, $row_cases);
+        }
+        complete_procedure($dbc);
+        
+        foreach($case_list as $case_row) {
+            $caseID = $case_row['SubmissionID'] ;
+            
 			$q_submission = "CALL spSubmissionGetInfo ($caseID);";
-			
 			$r_submission = @mysqli_query ($dbc, $q_submission);
 			
 			while($row_submission = mysqli_fetch_array($r_submission, MYSQLI_ASSOC)) {
@@ -112,30 +95,29 @@ if (!$error) {
 				// array_push($row_submission, $caseID);
 				array_push($submission_list, $row_submission);
 				foreach($submission_list as $submission_row) {
-					$case_title = $submission_row['IncidentTitle'] ;
+                    $case_title = $submission_row['IncidentTitle'] ;
 				}
-				
 				complete_procedure($dbc);
 			}
-			 $submission_fileIDs = array();
+			$submission_fileIDs = array();
 			$q_submission_file = "CALL spSubmissionGetFilesList ($caseID);";
 			$r_submission_file = @mysqli_query ($dbc, $q_submission_file);
 			while($row_submission_file = mysqli_fetch_array($r_submission_file, MYSQLI_ASSOC)) {
 				// download link taken from author_view_critical incident page
 				// echo '< name="ID" value="'.$row_submission_file['FileMetaDataID'].'"/>';
-				 // echo "<td><a href='download.php?fid=$file_ID'>Download</a></td>";
-				 //echo '</tr>';
+                // echo "<td><a href='download.php?fid=$file_ID'>Download</a></td>";
+                //echo '</tr>';
 				 
-				 array_push($submission_fileIDs, $row_submission_file);
+                array_push($submission_fileIDs, $row_submission_file);
 				
-				 foreach($submission_file_list as $submission_file_row) {
-					 $file_id = $submission_file_row['FileMetaDataID'] ;
-				 }
-				 complete_procedure($dbc);
+                foreach($submission_file_list as $submission_file_row) {
+                    $file_id = $submission_file_row['FileMetaDataID'] ;
+                }
+                complete_procedure($dbc);
 			}
 			$submission_file_list[$caseID] = $submission_fileIDs;
 			// array_push($submission_file_list, $submission_fileIDs);
-			}
+        }
 		  
 		  
 		$q_editors = " CALL spGetUsersEditorsList ;" ;
@@ -157,39 +139,35 @@ if (!$error) {
 		<?php
 		echo '<tr><td>';
 		foreach($submission_list as $submission_row) {
-					$caseID = $submission_row['SubmissionID'] ;
-					$case_name = $submission_row['IncidentTitle'] ;
-				
-					
-					
-		// looked at for assistance http://stackoverflow.com/questions/6112875/display-sql-data-in-a-list-with-check-box
-		// http://stackoverflow.com/questions/4997252/get-post-from-multiple-checkboxes
-		echo '<label for = "chk-case-'.$caseID.'">'.$case_name.'</label>';
-		echo '<input type="checkbox" id = "chk-case-'.$caseID.'" name="selected[]" value="'.$caseID.'"/>';
-		echo '</td>';
-		foreach($editor_list as $editor_row) {	
-		$editor_name = $editor_row['FullName'] ;
-		$editor_ID = $editor_row['UserID'];
-		// radio button goes here
-		echo '<td>';
-		echo '<label for = "radio-editor-'.$editor_ID.'">'.$editor_name.'</label>';
-		echo '<input type="radio" id = "radio-editor-'.$editor_ID.'" name="selected1" value="'.$editor_ID.'"/>';
-		echo '</td>';
-		// echo '<input type="radio" name="editor" value="'.$row_editors['UserID'].'"/>';
-		// case name editors file download
+            $caseID = $submission_row['SubmissionID'] ;
+            $case_name = $submission_row['IncidentTitle'] ;
+            
+            // looked at for assistance http://stackoverflow.com/questions/6112875/display-sql-data-in-a-list-with-check-box
+            // http://stackoverflow.com/questions/4997252/get-post-from-multiple-checkboxes
+            echo '<label for = "chk-case-'.$caseID.'">'.$case_name.'</label>';
+            echo '<input type="checkbox" id = "chk-case-'.$caseID.'" name="selected[]" value="'.$caseID.'"/>';
+            echo '</td>';
+            foreach($editor_list as $editor_row) {	
+                $editor_name = $editor_row['FullName'] ;
+                $editor_ID = $editor_row['UserID'];
+                // radio button goes here
+                echo '<td>';
+                echo '<label for = "radio-editor-'.$editor_ID.'">'.$editor_name.'</label>';
+                echo '<input type="radio" id = "radio-editor-'.$editor_ID.'" name="selected1" value="'.$editor_ID.'"/>';
+                echo '</td>';
+                // echo '<input type="radio" name="editor" value="'.$row_editors['UserID'].'"/>';
+                // case name editors file download
+            }
+            foreach($submission_file_list[$caseID] as $submission_file_row) {
+                $file_id = $submission_file_row['FileMetaDataID'] ;
+                $filename = $submission_file_row['FileName'] ;
+                $filesize = $submission_file_row['FileSize'] ;
+                echo '<td>';
+                create_download_link ($file_id, $filename, $filesize);
+                echo '</td>';
+                // download link goes here create_download_link($file_id, $filename, $filesize)
+            }
 		}
-		foreach($submission_file_list[$caseID] as $submission_file_row) {
-			$file_id = $submission_file_row['FileMetaDataID'] ;
-			$filename = $submission_file_row['FileName'] ;
-			$filesize = $submission_file_row['FileSize'] ;
-			echo '<td>';
-			create_download_link ($file_id, $filename, $filesize);
-			echo '</td>';
-			// download link goes here create_download_link($file_id, $filename, $filesize)
-		}
-		
-		}
-        
 		echo '</tr>';
 		?>
 		
