04f22535297a813c6ecaf43a6a39ba49e59cbb1a
Jeff Ballard <juggalobrotha@gmail.com>
2016-04-18 11:13:04 -0400

    Bug fixes for Eric

--- public_html/contact.php
+++ public_html/contact.php
@@ -15,45 +15,53 @@
  
  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
      require('./include_utils/login_functions.php');
-      require ('../mysqli_connect.php'); // Connect to the database
-      require ('./include_utils/procedures.php'); // complete_procedure()
-      require ('./include_utils/email_functions.php');
-      
-      if (!empty($_POST['Name'])) {
-          $Name = $_POST['Name'];
-      } else {
-          $errors[] = 'Please provide a name';
-      }
-      
-      if (!empty($_POST['Email'])) {
-          $Email = $_POST['Email'];
-      } else {
-          $errors[] = 'Please provide an email address';
-      }
-      
-      if (!empty($_POST['Phone'])) {
-          $Phone = $_POST['Phone'];
-      } else {
-          $Phone = '';
-      }
-      
-      if (!empty($_POST['Comment'])) {
-          $Comment = $_POST['Comment'];
-      } else {
-          $errors[] = 'Please provide a comment';
-      }
-      
-      if (empty($errors)) { // If everything's OK.
-        $Message = '<html><body>Editors,<br />&nbsp;&nbsp;A user made a comment on the JCI website.<br /><br />&nbsp;&nbsp;Name: '.$Name.'<br />&nbsp;&nbsp;Email: '.$Email.'<br />&nbsp;&nbsp;Phone: '.$Phone.'<br />&nbsp;&nbsp;Comment:<br />'.$Comment.'</body></html>';
-        
-        //Send the email
-        sendCommentEmail($dbc,$Message);
-        
-        mysqli_close($dbc); // Close the database connection.
-        
-        // Redirect:
-        redirect_user('index.php');
-      }
+     require ('../mysqli_connect.php'); // Connect to the database
+     require ('./include_utils/procedures.php'); // complete_procedure()
+     require ('./include_utils/email_functions.php');
+     
+     $Name = trim($_POST['Name']);
+     $Email = trim($_POST['Email']);
+     $Phone = trim($_POST['Phone']);
+     $Comment = trim($_POST['Comment']);
+     
+     if (empty($Name)) {
+         $errors[] = 'Please provide a name';
+     }
+     
+     if (!empty($Email)) {
+        if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Please provide a valid email address';
+        }
+     } else {
+         $errors[] = 'Please provide an email address';
+     }
+     
+     if (!empty($Phone)) {
+         if (strlen($Phone) < 14) {
+             $errors[] = 'Please provide a full phone number';
+         } else {
+             $PhoneTmp = str_replace('(', '', str_replace(')', '', str_replace(' ', '', str_replace('-', '', $Phone))));
+             if ((strlen($PhoneTmp) != 10) || (!is_numeric($PhoneTmp))) {
+                 $errors[] = 'Please provide a valid phone number';
+             }
+         }
+     }
+     
+     if (empty($Comment)) {
+         $errors[] = 'Please provide a comment';
+     }
+     
+     if (empty($errors)) { // If everything's OK.
+       $Message = '<html><body>Editors,<br />&nbsp;&nbsp;A user made a comment on the JCI website.<br /><br />&nbsp;&nbsp;Name: '.$Name.'<br />&nbsp;&nbsp;Email: '.$Email.'<br />&nbsp;&nbsp;Phone: '.$Phone.'<br />&nbsp;&nbsp;Comment:<br />'.$Comment.'</body></html>';
+       
+       //Send the email
+       sendCommentEmail($dbc,$Message);
+       
+       mysqli_close($dbc); // Close the database connection.
+       
+       // Redirect:
+       redirect_user('index.php');
+     }
  }
 ?>
 <div class="content">
@@ -80,9 +88,9 @@
                 </div>
                 <div class="box editor_alt">
                     <form method="post">
-                        <input type="text" class="regular inputForm" placeholder="Name" name="Name" width="100%" value="<?php echo $Name; ?>">
-                        <input type="text" class="regular inputForm" placeholder="Email" name="Email" width="100%" value="<?php echo $Email; ?>">
-                        <input type="text" class="regular inputForm" placeholder="Phone" name="Phone" width="100%" value="<?php echo $Phone; ?>">
+                        <input type="text" class="regular inputForm" placeholder="Name" name="Name" maxlength="50" width="100%" value="<?php echo $Name; ?>">
+                        <input type="text" class="regular inputForm" placeholder="Email" name="Email" maxlength="200" width="100%" value="<?php echo $Email; ?>">
+                        <input type="text" class="regular inputForm" placeholder="Phone: (###) ###-####" name="Phone" maxlength="14" width="100%" value="<?php echo $Phone; ?>">
                         <textarea placeholder="Comments" rows="10" name="Comment"><?php echo $Comment; ?></textarea>
                         <button class="editor buttonform" type="submit">Send</button>
                     </form>
